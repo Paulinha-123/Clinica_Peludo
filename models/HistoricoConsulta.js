135dd62b638e535626d5dbdfcd96eb5a8e3c645a
Paulo Davi <paulo.araujo@cobli.co>
2025-03-31 20:01:44 -0300

    feat: fix local files

--- models/HistoricoConsulta.js
+++ models/HistoricoConsulta.js
@@ -1,14 +1,19 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const historicoConsultaSchema = new mongoose.Schema({
-   
-    usuarioId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    data: { type: Date, required: true },
-    descricao: { type: String, required: true },
-    observacao: { type: String }
-
+  usuarioId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  data: { type: Date, required: true },
+  descricao: { type: String, required: true },
+  observacao: { type: String },
 });
 
-const HistoricoConsulta = mongoose.model('HistoricoConsulta', historicoConsultaSchema);
+const HistoricoConsulta = mongoose.model(
+  "HistoricoConsulta",
+  historicoConsultaSchema
+);
 
 module.exports = HistoricoConsulta;
